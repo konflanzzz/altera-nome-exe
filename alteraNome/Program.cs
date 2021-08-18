10c3690e2daead17e914602e95b8bf5ba9524efb
Fernando Konflanz <konflanz.info@gmail.com>
2021-08-18 18:30:08 -0300

    Update 18082021
    - Adicionado tratamentos de erros e capturas de Exceptions

--- alteraNome/Program.cs
+++ alteraNome/Program.cs
@@ -10,24 +10,72 @@ namespace alteraNome
         {
             // chamada da funcao principal
             string novoNome = "";
+            string caminhoTemp = "";
+            string caminhoRemessas = "";
+            string[] linhas = new string[2] { "", "" };
 
             // array que vai armazenar os diretorios
-            string[] linhas = File.ReadAllLines(@"./config.txt");
+            try 
+            {
+                linhas = File.ReadAllLines(@"./config.txt"); 
+            }
+
+            catch (Exception ex) // e se o arquivo nao estiver junto ao .exe?
+            {
+                Console.WriteLine("Houve um problema ao ler o arquivo confif.txt: ");
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                return;
+            }
+            
 
             // variaveis recebem o diretorio da pasta Temp e da Pasta Remessas do Client Suite
-            string caminhoTemp = linhas[0];
-            string caminhoRemessas = linhas[1];
+            if (linhas[0] != "") 
+            { 
+                caminhoTemp = linhas[0];
+            } 
+            else
+            {
+                Console.WriteLine("Favor infomar o caminho do diretorio Temp no arquivo config.txt");
+                Console.ReadLine();
+                return;
+            }
+
+            if (linhas[1] != "")
+            {
+                caminhoRemessas = linhas[1];
+            }
+            else
+            {
+                Console.WriteLine("Favor infomar o caminho do diretorio Remessas no arquivo config.txt");
+                Console.ReadLine();
+                return;
+            }
 
             // verifica se o diretorio da pasta Temp existe
             if (Directory.Exists(caminhoTemp) == false)
             {
-                Console.WriteLine("Nao foi possivel encontrar o diretorio Temp.");
+                Console.WriteLine("Diretorio temporario nao foi encontrado.");
+
+                try
+                {
+                    Directory.CreateDirectory(caminhoTemp);
+                    Console.WriteLine("Diretorio temporario foi criado em: ", caminhoTemp);
+                }
+                catch(Exception ex) // caso o arquivo config.txt nao esteja presente junto ao .exe
+                {
+                    Console.WriteLine("Houve um problema ao criar o diretorio Temp: ");
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                    return;
+                }
+
             }
 
             // verifica se o diretorio da pasta Remessas está acessivel / existe 
             if (Directory.Exists(caminhoRemessas) == false)
             {
-                Console.WriteLine("Nao foi possivel encontrar o diretorio Remessas.");
+                Console.WriteLine("Diretorio Remessas nao foi encontrado.");
             }
 
             if (Directory.Exists(caminhoTemp))
@@ -35,62 +83,83 @@ namespace alteraNome
                 // armazena os arquivos em um array
                 string[] arquivos = Directory.GetFiles(caminhoTemp);
 
+                // percorre todos os nomes dos arquivos, armazenados no array, obtidos no diretorio
                 foreach (string n in arquivos)
                 {
                     // obtem o nome de cada um dos arquivos capturados
-                    string nomeArquivo = Path.GetFileNameWithoutExtension(n);
+                    // armazena o nome do arquivo com extensao
+                    string nomeArquivo = Path.GetFileName(n);
 
-                    // caso seja um arquivo de emissao, altera o nome mas mantem a numeracao informada
-                    if (Regex.IsMatch(nomeArquivo, @"^[0-9]+$"))
+                    if (nomeArquivo.EndsWith(".txt"))
                     {
-                        novoNome = "NFEEMISSAO_" + nomeArquivo + ".txt";
-                    }
+                        // armazena o nome do arquivo sem extensao
+                        nomeArquivo = Path.GetFileNameWithoutExtension(n);
 
-                    else
-                    {
-                        switch (nomeArquivo)
+                        // caso seja um arquivo de emissao, altera o nome mas mantem a numeracao informada
+                        if (Regex.IsMatch(nomeArquivo, @"^[0-9]+$"))
                         {
-                            // altera o nome para cancelamento
-                            case "CANC":
-                                novoNome = "NFECANC_.txt";
-                                break;
-
-                            // altera o nome para carta de correcao
-                            case "CCE":
-                                novoNome = "NFECCE_.txt";
-                                break;
-
-                            // altera o nome para consulta de situacao da NFe
-                            case "SITU":
-                                novoNome = "NFESITUACAO_.txt";
-                                break;
-
-                            // altera o nome para agendamento de envio de email da NFe
-                            case "MAIL":
-                                novoNome = "NFEREENVIOEMAIL_.txt";
-                                break;
-
-                            // altera o nome para reimpressao da NFe
-                            case "RIMP":
-                                novoNome = "NFEREIMPRIME_.txt";
-                                break;
-
-                            // altera o nome para reimpressao de evento da NFe
-                            case "REVE":
-                                novoNome = "NFEREIMPRIMEEVENTO_.txt";
-                                break;
-
-                            // altera o nome para previa da DANFE
-                            case "PREV":
-                                novoNome = "NFEPREVIA_.txt";
-                                break;
-
+                            // caso seja um arquivo de emissao, mantem a numeracao e adiciona a nomenclatura
+                            novoNome = "NFEEMISSAO_" + nomeArquivo + ".txt";
                         }
-                    }
 
-                    // para cada um dos arquivos do array, move para a pasta Remessas, alterando o nome
-                    string nomeFinalArquivo = Path.Combine(caminhoRemessas, novoNome);
-                    File.Move(n, nomeFinalArquivo, true);
+                        else
+                        {
+                            switch (nomeArquivo.ToUpper())
+                            {
+                                // altera o nome para cancelamento
+                                case "CANC":
+                                    novoNome = "NFECANC_.txt";
+                                    break;
+
+                                // altera o nome para carta de correcao
+                                case "CCE":
+                                    novoNome = "NFECCE_.txt";
+                                    break;
+
+                                // altera o nome para consulta de situacao da NFe
+                                case "SITU":
+                                    novoNome = "NFESITUACAO_.txt";
+                                    break;
+
+                                // altera o nome para agendamento de envio de email da NFe
+                                case "MAIL":
+                                    novoNome = "NFEREENVIOEMAIL_.txt";
+                                    break;
+
+                                // altera o nome para reimpressao da NFe
+                                case "RIMP":
+                                    novoNome = "NFEREIMPRIME_.txt";
+                                    break;
+
+                                // altera o nome para reimpressao de evento da NFe
+                                case "REVE":
+                                    novoNome = "NFEREIMPRIMEEVENTO_.txt";
+                                    break;
+
+                                // altera o nome para previa da DANFE
+                                case "PREV":
+                                    novoNome = "NFEPREVIA_.txt";
+                                    break;
+                            }
+                        }
+
+                        // para cada um dos arquivos do array, altera o nome final do arquivo
+                        string nomeFinalArquivo = Path.Combine(caminhoRemessas, novoNome);
+                        
+                        try
+                        {   
+                            // para cada um dos arquivos do array, move para a pasta remessas
+                            File.Move(n, nomeFinalArquivo, true);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Houve um problema ao mover o arquivo: ");
+                            Console.WriteLine(ex.Message);
+                            Console.ReadLine();
+                            return;
+                        }
+
+                    }
                 }
             }
             else
